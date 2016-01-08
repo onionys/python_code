462a84425d6413772ceb40a3c33ff269c8815839
onionys <onionys@gmail.com>
2016-01-08 04:40:51 +0000

    modified:   env_config/02-install_python3.sh 	new file:   env_config/03-install_tools.sh

--- RasPi/env_config/02-install_python3.sh
+++ RasPi/env_config/02-install_python3.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 echo "------------------------------"
-echo "install common tools"
-echo "------------------------------"
-apt-get install vim ctags automake subversion  gedit
-
-echo "------------------------------"
 echo "install python3 Dev tools"
 echo "------------------------------"
 apt-get install ipython3-notebook python3-dev python3-pip geany geany-plugin-py bpython3
+
 
 echo "---------------------------------"
 echo "install python3 i2c, spi, flask  "
 echo "---------------------------------"
 apt-get install python3-smbus python3-spidev python3-flask
 
-echo "------------------------------"
-echo "install chinese font and input"
-echo "------------------------------"
-apt-get install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy scim scim-tables-zh scim-chewing
 
+echo "----------------------------------"
+echo " pip install python class packages"
+echo "----------------------------------"
+#pip install numpy scipy sympy pandas requests lxml
+
+#apt-get install liblxml2-dev libxslt1-dev
+#pip3 install lxml
+
+#apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+#pip install scipy
+
+#apt-get install python3-numpy python3-scipy python3-matplotlib python3-pandas python3-pandas-lib python3-lxml python3-requests 
+#pip install sympy
 
