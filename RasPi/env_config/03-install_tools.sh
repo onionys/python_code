048a9d2c36d2fe77f0b9212f77204eb576a75fa9
onionys <onionys@gmail.com>
2016-01-11 05:19:34 +0000

    modified:   03-install_tools.sh

--- RasPi/env_config/03-install_tools.sh
+++ RasPi/env_config/03-install_tools.sh
@@ -5,3 +5,8 @@ echo "install common tools"
 echo "------------------------------"
 apt-get install -y vim ctags automake subversion  gedit
 
+echo "-------------------------------"
+echo "install chinese fonts and input"
+echo "-------------------------------"
+apt-get install -y ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy scim scim-tables-zh scim-chewing
+
